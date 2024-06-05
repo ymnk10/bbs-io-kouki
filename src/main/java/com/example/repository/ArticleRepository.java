619ae78ef4d1d6a8f7d746a1570192f752d8f79c
ymnk10 <kseisaimin10@gmail.com>
2024-06-05 14:19:30 +0900

    create repository and domain

---
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -0,0 +1,27 @@
+package com.example.repository;
+
+import com.example.domain.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+public class ArticleRepository {
+
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    private static final RowMapper<Article> ARTICLE_ROW_MAPPER = new BeanPropertyRowMapper<>(Article.class);
+
+    public List<Article> findAll(){
+        String sql = "SELECT (id, name, content) FROM articles ORDER BY id DESC;";
+        List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
+        return articleList;
+    }
+}
