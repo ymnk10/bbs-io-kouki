32e58eb3e462c63455028d3135f8c85ce7d02d14
ymnk10 <kseisaimin10@gmail.com>
2024-06-05 15:25:46 +0900

    create finction of posting

--- src/main/java/com/example/controller/ShowBbsController.java
+++ src/main/java/com/example/controller/ShowBbsController.java
@@ -1,12 +1,16 @@
 package com.example.controller;
 
+
 import com.example.domain.Article;
+import com.example.form.ArticleForm;
 import com.example.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.BeanUtils;
 
 import java.util.List;
 
@@ -26,5 +30,13 @@ public class ShowBbsController {
     List<Article> articles = articleRepository.findAll();
     model.addAttribute("articles", articles);
     return "index";
+  }
+
+  @PostMapping("/insert-article")
+  public String insertArticle(ArticleForm form, Model model) {
+    Article article = new Article();
+    BeanUtils.copyProperties(form, article);
+    articleRepository.insertArticle(article);
+    return "redirect:/show";
   }
 }
